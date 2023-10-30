692d7ed4f514e51057da63a5d163b0ac8847db34
kavyavinayak <143301095+kavyavinayak@users.noreply.github.com>
2023-10-29 18:44:55 -0700

    Create mission_08_09.py (#30)
    * Create mission_08_09.py
    
    * Update mission_08_09.py

---
+++ src/mission_08_09.py
@@ -0,0 +1,49 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor
+from pybricks.parameters import Port
+from pybricks.tools import wait
+
+class Mission_08_09:
+    def __init__(self, left_motor, right_motor):
+        self.left_motor = left_motor
+        self.right_motor = right_motor
+    
+    def forward(self, speed,distance):
+        cm = 27.6
+        self.left_motor.run_angle(speed, distance/cm * 360, wait=False)
+        self.right_motor.run_angle(speed, distance/cm* -360, wait=False)
+        while not self.left_motor.done() or not self.right_motor.done():
+            wait(10)
+    
+    def backwards(self, speed,distance):
+        self.forward(speed,-distance)
+    
+    def right_turn(self, degree):
+        self.left_turn((-1)*degree)
+    
+    def left_turn(self, degree):
+        distance_between_wheels = 143
+        pi = 3.14159
+        wheel_diameter = 88
+        # calculating the wheel circumfrence - wheel diameter multiplied by pi
+        wheel_circumfrence = wheel_diameter * pi
+        # calculating the distance the wheel needs to spin
+        distance_to_rotate = distance_between_wheels * pi / 360 * degree 
+        # calculating how many times the wheel needs to turn, and then converts it to degrees
+        rotation_angle = distance_to_rotate / wheel_circumfrence * 360
+        #reset_angle after every movementt makes the robot's movements more accurate
+        self.left_motor.reset_angle(0)
+        self.right_motor.reset_angle(0)
+    
+        # wait = false makes it so that both motors start running at the same time
+        # if wait = true then the program would wait for one motor to finish turning
+        speed = 100
+    
+        self.left_motor.run_angle(speed, rotation_angle, wait=False)
+        self.right_motor.run_angle(speed, rotation_angle, wait=False) 
+        while not self.left_motor.done() or not self.right_motor.done():
+            wait(10)
+    
+    def run(self):
+       self.forward(200,45)
+       self.backwards(1000, 45)
