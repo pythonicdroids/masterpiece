9bac206c100f7187f89770956c8a115f26f1f3e2
maximilianyimozeng <142680437+maximilianyimozeng@users.noreply.github.com>
2023-11-20 20:09:28 -0800

    Update run_06_max.py

--- src/run_06_max.py
+++ src/run_06_max.py
@@ -10,12 +10,7 @@ class Run_06:
         self.drive_base.straight(220)
         self.drive_base.turn(60)
         self.drive_base.straight(130)
-        
-        # retry once to make sure the speakers are indeed turned
-        #self.attachment_motor.run_angle(5000, speaker_turn_angle/c_cm * 360)
-        #self.attachment_motor.run_angle(1500, -speaker_turn_angle/c_cm * 360)
-        #self.attachment_motor.run_angle(5000, speaker_turn_angle/c_cm * 360)
-        
+               
         self.drive_base.straight(-100)
         self.drive_base.turn(90)
         self.drive_base.straight(300)
