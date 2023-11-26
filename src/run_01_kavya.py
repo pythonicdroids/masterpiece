ce078973515338af03531d19d0633546f52c7393
droidpythonic <droidpythonic@gmail.com>
2023-11-25 22:47:32 -0800

    checkpoints for kavya and ishanvi

--- src/run_01_kavya.py
+++ src/run_01_kavya.py
@@ -5,8 +5,9 @@ from pybricks.tools import wait
 from mission_base import MissionBase
 
 class Run_01(MissionBase):
-    def __init__(self, drive_base, left_motor, right_motor, side_motor):
+    def __init__(self, drive_base, left_motor, right_motor, side_motor, attachment_motor):
         super().__init__(left_motor, right_motor, side_motor)
+        self.attachment_motor = attachment_motor
         self.drive_base = drive_base
     
     def nudge_lever(self):
@@ -30,3 +31,13 @@ class Run_01(MissionBase):
     def push_camera(self):
        self.backwards(200,45)
        self.forward(1000, 45)
+
+    def mission_01(self):
+        turn_angle = 450
+        self.drive_base.straight(185)
+        self.attachment_motor.run_angle(100, turn_angle)
+        self.drive_base.straight(-160)
+        wait(1000)
+        self.drive_base.straight(-80)
+        self.attachment_motor.run_angle(100, -turn_angle)
+        self.drive_base.straight(-150)
