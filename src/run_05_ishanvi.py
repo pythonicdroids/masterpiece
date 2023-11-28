d9bd804cbf265cd733e60ea34c91fb8c3c0821ca
Aron Pany <aron.pany@gmail.com>
2023-11-27 18:11:52 -0800

    run05 updates to speed up going to the other side post chicken mission

--- src/run_05_ishanvi.py
+++ src/run_05_ishanvi.py
@@ -1,4 +1,5 @@
 from robot import Robot
+from pybricks.parameters import Stop
 
 class Run_05():
     def __init__(self, robot):
@@ -20,18 +21,15 @@ class Run_05():
         self.robot.backwards(300, 48)
 
     def run2(self):
+        self.robot.drive_base.settings(straight_speed = 300, turn_rate = 300)
         self.robot.drive_base.straight(235)
         self.robot.side_motor.run_angle(100, 100)
         self.robot.drive_base.straight(67)
-        self.robot.attachment_motor.run_angle(400, 2600)
+        self.robot.attachment_motor.run_angle(800, 2600)
         self.robot.drive_base.straight(-200)
         self.robot.drive_base.turn(-38)
         self.robot.drive_base.straight(120)
         self.robot.drive_base.turn(8)
-        self.robot.drive_base.curve(1200, -4)
-        self.robot.drive_base.straight(80)
-        self.robot.drive_base.straight(-20)
-        self.robot.drive_base.turn(2)
-        self.robot.drive_base.straight(500)
-        self.robot.drive_base.turn(-40)
-        self.robot.drive_base.straight(200)
+        self.robot.drive_base.settings(straight_speed = 300, straight_acceleration = [400, 400], turn_rate = 300)
+        self.robot.drive_base.curve(1200, -10, then = Stop.COAST)
+        self.robot.drive_base.curve(3100, -20)
